c6558a1f1c1017a84b56a1fa145522e94593aa75
PROMLEE <donghoon099@naver.com>
2024-01-16 14:14:49 +0900

    fix: 로그인 페이지 크기 조절 - 로그인 페이지 크기 조절 Related to: #1

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,10 @@
-import styled from 'styled-components';
 import { Loginbutton, Mobiletop } from '../../components';
 
 export default function Login() {
   return (
-    <LoginBackground>
+    <>
       <Mobiletop pagename="로그인" />
       <Loginbutton />
-    </LoginBackground>
+    </>
   );
 }
-const LoginBackground = styled.div`
-  position: relative;
-  height: 100%;
-  width: 100%;
-`;
