dc9b542f2ab24a5c3da59d9d67ae0982c53311a1
장난영 <nanyoung1151@naver.com>
2024-01-20 10:53:21 +0900

    Feat: 글꼴 변경

--- src/components/SearchFile/SearchResultList.tsx
+++ src/components/SearchFile/SearchResultList.tsx
@@ -5,154 +5,132 @@ import { SearchDummy } from './SearchDummy';
 const SearchResultList = () => {
 
     return (
-    <Wrap>
-      <SearchResults>
-        {SearchDummy.map((i) => (
-          <SearchResultItem key={i.storeName}>
-            <ContentContainer>
-              <StoreImg src="assets/Frame.png" />
-              <TextContainer>
-              <StoreName>{i.storeName}</StoreName>
-              <StoreProduct>{i.storeProduct}</StoreProduct>
-              <StoreAddress>{i.address}</StoreAddress>
-              </TextContainer>
-            </ContentContainer>
-            <BookmarkIcon src="assets/bookmark.png" alt="북마크 아이콘" />
-          </SearchResultItem>
-        ))}
-      </SearchResults>
-    </Wrap>
+      <SearchResult>
+      {SearchDummy.map((i) => (
+        <Container key={i.storeName}>
+          <SearchText>{i.storeName}</SearchText>
+          <ProductText>{i.storeProduct}</ProductText>
+          <AddressFrame>
+            <AddressText>{i.address}</AddressText>
+            <NumReview>
+              <p style={{textAlign: 'right', color: '#E0E0E0', fontSize: 10, fontFamily: 'Noto Sans', fontWeight: '400', wordWrap: 'break-word'}}>(42)</p>
+            </NumReview>
+          </AddressFrame>
+          <StoreFrame>
+            <StoreImg src={`${process.env.PUBLIC_URL}assets/Search/${i.photo}`} alt={`${i.storeName}의 이미지`} />
+          </StoreFrame>
+          <BookMarkFrame>
+          <BookMarkIcon src={`${process.env.PUBLIC_URL}/assets/Search/bookmark.png`} alt="북마크아이콘" />
+          </BookMarkFrame>
+        </Container>
+      ))}
+      </SearchResult>
     );
 };
 
-
-const Wrap = styled.div`
-  width: calc(100vw - 10%);
-  margin : 10%;
-  @media (max-width: 768px) {
-    width: calc(100vw - 5%);
-    margin-left: 5%;
-  }
-`;
-
-const SearchResults = styled.div`
-
-  display: flex;
-  flex-direction: column;
-  height: 30rem;
-  width: 100%;
-  padding: 3rem 0 3rem 1rem;
-  overflow-y: auto;
-
-  /* 인터넷 익스플로러를 위한 스타일 */
-  -ms-overflow-style: none;
-
-  /* 파이어폭스를 위한 스타일 */
-  scrollbar-width: none;
-
-  /* 웹킷(크롬, 사파리, 새로운 엣지) 브라우저를 위한 스타일 */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  @media (max-width: 768px) {
-    height: 55rem;
-    padding: 5rem 0 5rem 2rem;
-  }
-`;
-
-
-const SearchResultItem = styled.div`
-  width: 723px;
+const Container = styled.div`
+  width: 924px;
   height: 162px;
-  flex-shrink: 0;
+  position: relative;
+  background: white;
   border-radius: 8px;
-  border: 1px solid var(--light-gray, #F2F2F2);
-  background: #FFF;
+  overflow: hidden;
+  border: 1px #F2F2F2 solid;
+  margin-bottom: 24px;
+`;
+
+const SearchResult = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 331px;
+  
+`;
 
+const SearchText = styled.div`
+  left: 178px;
+  top: 13px;
+  position: absolute;
+  color: #0B5C71;
+  font-size: 16px;
+  font-family: 'Noto Sans';
+  font-weight: 500;
+  word-wrap: break-word;
+`;
 
-  & > h3 {
-    font-size: 1.4rem;
-    margin: 1rem;
-    width: 45px
-    height: 22px
-    top: 13px
-    left: 178px
+const ProductText = styled.div`
+  left: 178px;
+  top: 43px;
+  position: absolute;
+  color: #565656;
+  font-size: 12px;
+  font-family: 'Noto Sans';
+  font-weight: 400;
+  word-wrap: break-word;
+`;
 
-  }
-  & > p {
-    font-size: 1rem;
-    margin: 2rem 0 0 1rem;
-    color: rgba(86, 86, 86, 1);
-  }
+const AddressFrame = styled.div`
+  left: 178px;
+  top: 106px;
+  position: absolute;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: inline-flex;
+`;
+
+const AddressText = styled.div`
+  color: #565656;
+  font-size: 10px;
+  font-family: 'Noto Sans';
+  font-weight: 400;
+  word-wrap: break-word;
+`;
+
+const NumReview = styled.div`
+  height: 20px;
+  justify-content: flex-start;
+  align-items: center;
+  display: inline-flex;
+`;
+
+const StoreFrame = styled.div`
+  width: 162px;
+  height: 162px;
+  left: 0;
+  top: 0;
+  position: absolute;
+  background: white;
 `;
 
 const StoreImg = styled.img`
-width: 162px
-height: 162px
-
-`;
-
-const BookmarkIcon = styled.img`
-  width: 1.5rem;
-  height: 2rem;
-  position: absolute;
+  width: 162px;
+  height: 162px;
+  left: 0;
   top: 0;
-  right: 0;
-
+  position: absolute;
+  background: #D9D9D9;
 `;
 
-const StoreName = styled.p`
-width: 45px
-height: 22px
-top: 13px
-left: 178px
-font-family: Noto Sans;
-font-size: 16px;
-font-weight: 500;
-line-height: 22px;
-letter-spacing: 0em;
-text-align: left;
 
-
+const BookMarkFrame = styled.div`
+  padding: 16px;
+  left: 880px;
+  top: 0;
+  position: absolute;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 10px;
+  display: inline-flex;
 `;
 
-const StoreProduct = styled.p`
-width: 166px
-height: 16px
-top: 43px
-left: 178px
-font-family: Noto Sans;
-font-size: 12px;
-font-weight: 400;
-line-height: 16px;
-letter-spacing: 0em;
-text-align: left;
-
-`;
-
-const StoreAddress = styled.p`
-width: 40px
-height: 14px
-font-family: Noto Sans;
-font-size: 10px;
-font-weight: 400;
-line-height: 14px;
-letter-spacing: 0em;
-text-align: left;
-
-`;
-
-const ContentContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const TextContainer = styled.p`
-  flex-direction: column;
-  marginLeft: 1rem;
+const BookMarkIcon = styled.img`
+  width: 11.72px;
+  height: 15px;
+  position: relative;
 `;
 
 
