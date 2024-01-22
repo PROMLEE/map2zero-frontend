879ec27a4be1de5d5a1e1f986dfaf965a377883a
PROMLEE <donghoon099@naver.com>
2024-01-22 14:12:32 +0900

    fix: 로그인 페이지 수정 -  media query: min width → max width - png → svg 변환 - 로그인 버튼 png → css로 제작 Related to: [fix] 소규모 이슈 관리 #26

--- src/components/Login/Loginbutton.tsx
+++ src/components/Login/Loginbutton.tsx
@@ -17,62 +17,131 @@ export default function Loginbutton() {
   };
 
   return (
-    <>
-      <LoginBox>
-        <Logo src={`${process.env.PUBLIC_URL}/assets/Login/login_logo.png`} />
-        <LoginText src={`${process.env.PUBLIC_URL}/assets/Login/login_text.png`} />
-        <a onClick={loginKaKao}>
-          <LoginImg src={`${process.env.PUBLIC_URL}/assets/Login/kakao_login_button.png`} />
-        </a>
-        <a onClick={loginNaver}>
-          <LoginImg src={`${process.env.PUBLIC_URL}/assets/Login/naver_login_button.png`} />
-        </a>
-      </LoginBox>
-    </>
+    <LoginBox>
+      <Logo src={`${process.env.PUBLIC_URL}/assets/Navbar/logo.png`} />
+      <LoginText>Map to Zero Waste</LoginText>
+      <LoginMiniText>
+        <Line src={`${process.env.PUBLIC_URL}/assets/Login/line.svg`} />
+        맵투제로 로그인하기
+        <Line src={`${process.env.PUBLIC_URL}/assets/Login/line.svg`} />
+      </LoginMiniText>
+      <LoginImgs>
+        <LoginImg onClick={loginKaKao} $background="#FEE500" $color="black">
+          <img src={`${process.env.PUBLIC_URL}/assets/Login/kakao.svg`} />
+          카카오톡 로그인
+        </LoginImg>
+        <LoginImg onClick={loginNaver} $background="#03C85A" $color="white">
+          <img src={`${process.env.PUBLIC_URL}/assets/Login/naver.svg`} />
+          네이버 로그인
+        </LoginImg>
+      </LoginImgs>
+    </LoginBox>
   );
 }
 
 const LoginBox = styled.div`
-  margin: 0;
+  margin-top: 23.4rem;
   display: flex;
   flex-direction: column;
   align-items: center;
-  @media screen and (min-width: 768px) {
-    border-radius: 2.4rem;
+  @media screen and (max-width: 768px) {
+    margin-top: 40rem;
+    width: 100%;
   }
 `;
 const Logo = styled.img`
-  width: 69.25rem;
-  height: 48.5rem;
-  margin-top: 40rem;
-  margin-bottom: 18.75rem;
-  @media screen and (min-width: 768px) {
-    width: 27.7rem;
-    height: 19.4rem;
-    margin-top: 25.4rem;
-    margin-bottom: 2.2rem;
+  width: 19rem;
+  height: 14.8rem;
+  @media screen and (max-width: 768px) {
+    width: 47.5rem;
+    height: 37rem;
   }
 `;
-const LoginText = styled.img`
-  margin-bottom: 8rem;
-  @media screen and (min-width: 768px) {
-    margin-bottom: 3.2rem;
-    height: 1.2rem;
+const LoginText = styled.div`
+  margin-top: 2.4rem;
+  color: #565656;
+  font-family: 'Noto Sans KR';
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  @media screen and (max-width: 768px) {
+    margin-top: 6rem;
+    font-size: 4rem;
   }
 `;
-const LoginImg = styled.img`
-  width: 81.75rem;
-  height: 15rem;
-  margin-bottom: 4rem;
-  border-radius: 2rem;
-  @media screen and (min-width: 768px) {
-    &:hover {
-      cursor: pointer;
-      box-shadow: 0rem 0rem 0.4rem 0rem rgba(0, 0, 0, 0.25);
+const LoginMiniText = styled.div`
+  margin-top: 4rem;
+  display: flex;
+  width: 92.4rem;
+  align-items: center;
+  gap: 0.8rem;
+  color: #565656;
+  font-family: 'Noto Sans KR';
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  @media screen and (max-width: 768px) {
+    width: 81.75rem;
+    margin-top: 18.25rem;
+    font-size: 2.5rem;
+    gap: 2rem;
+  }
+`;
+const Line = styled.img`
+  width: 41.1rem;
+  @media screen and (max-width: 768px) {
+    width: 28.125rem;
+  }
+`;
+const LoginImgs = styled.div`
+  margin-top: 3.2rem;
+  display: flex;
+  width: 34.8rem;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+  margin-bottom: 10rem;
+  @media screen and (max-width: 768px) {
+    margin-top: 8rem;
+    width: 100%;
+    gap: 4rem;
+    margin-bottom: 25rem;
+  }
+`;
+
+const LoginImg = styled.div<{ $background: string; $color: string }>`
+  display: flex;
+  width: 34.8rem;
+  height: 6rem;
+  padding: 1rem 6rem;
+  justify-content: center;
+  align-items: center;
+  gap: 2.2rem;
+  border-radius: 0.8rem;
+  background: ${(props) => props.$background};
+  color: ${(props) => props.$color};
+  font-family: 'Noto Sans KR';
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: normal;
+  &:hover {
+    cursor: pointer;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.1) 100%), ${(props) => props.$background};
+    box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
+    &:active {
+      box-shadow:
+        0px 0px 10px 0px rgba(0, 0, 0, 0.25) inset,
+        0px 0px 4px 0px rgba(0, 0, 0, 0.25);
     }
-    height: 6rem;
-    width: 34.8rem;
-    border-radius: 0.8rem;
-    margin-bottom: 1.6rem;
+  }
+  @media screen and (max-width: 768px) {
+    width: 80%;
+    height: 15rem;
+    padding: 2.5rem 15rem;
+    font-size: 3.5rem;
+    gap: 5.5rem;
+    border-radius: 2rem;
   }
 `;
