9a97f57760dace620592c66eb6902724d48b4871
PROMLEE <donghoon099@naver.com>
2024-01-27 22:17:21 +0900

    feat: 마커 클릭시 정보 표시 Related to: [new feature] 맵 페이지 생성 #38

---
+++ src/components/Map/MarkerInfo.tsx
@@ -0,0 +1,129 @@
+import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const MarkerInfo = () => {
+  return (
+    <MarkerBox>
+      <Title>매장명</Title>
+      <InfoBox>
+        <StoreImg />
+        <TextBox>
+          <TopBox>
+            <StarBox>
+              <Star src={`${process.env.PUBLIC_URL}/assets/StoreDetail/star_full.svg`} />
+              <Star src={`${process.env.PUBLIC_URL}/assets/StoreDetail/star_full.svg`} />
+              <Star src={`${process.env.PUBLIC_URL}/assets/StoreDetail/star_full.svg`} />
+              <Star src={`${process.env.PUBLIC_URL}/assets/StoreDetail/star_empty.svg`} />
+              <Star src={`${process.env.PUBLIC_URL}/assets/StoreDetail/star_empty.svg`} />
+              <Reviewnum>(32)</Reviewnum>
+            </StarBox>
+            <BookmarkImg src={`${process.env.PUBLIC_URL}/assets/StoreDetail/bookmark.png`} />
+          </TopBox>
+          <AdressBox>매장 주소</AdressBox>
+          <BottomButton>매장 상세보기 {'>>'}</BottomButton>
+        </TextBox>
+      </InfoBox>
+    </MarkerBox>
+  );
+};
+const MarkerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 45rem;
+  height: 31.6rem;
+  background-image: url('/assets/Map/balloon.svg');
+  background-size: cover;
+`;
+const Title = styled.div`
+  color: var(--Main, #0b5c71);
+  font-family: 'Noto Sans KR';
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  font-weight: bold;
+  margin-top: 2.4rem;
+  margin-left: 2.4rem;
+`;
+const InfoBox = styled.div`
+  display: flex;
+`;
+const StoreImg = styled.div`
+  background-image: url('/assets/Navbar/logo.png');
+  width: 20.1rem;
+  height: 20.1rem;
+  background-size: 100% 100%;
+  margin-top: 1.6rem;
+  margin-left: 2.4rem;
+  background-color: #d9d9d9;
+  border-radius: 1rem;
+`;
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.6rem;
+  margin-left: 2.4rem;
+  width: 17.7rem;
+  height: 2rem;
+`;
+const TopBox = styled.div`
+  display: flex;
+  width: 17.7rem;
+  justify-content: space-between;
+`;
+const StarBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Star = styled.img`
+  width: 1.4rem;
+  height: 1.4rem;
+`;
+
+const Reviewnum = styled.div`
+  color: var(--gray, #e0e0e0);
+  text-align: right;
+  font-family: 'Noto Sans KR';
+  font-size: 1.2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+const BookmarkImg = styled.img`
+  width: 1.4rem;
+  height: 1.8rem;
+`;
+const AdressBox = styled.div`
+  margin-top: 1.6rem;
+  width: 14.4rem;
+  height: 11.9rem;
+  flex-shrink: 0;
+  color: #565656;
+  font-family: 'Noto Sans KR';
+  font-size: 1.2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+const BottomButton = styled.div`
+  margin-top: 1.4rem;
+  margin-left: auto;
+  display: flex;
+  width: 15.2rem;
+  height: 3.2rem;
+  padding: 0.8rem 1.6rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 1.6rem;
+  background: var(--Point2, #74b69d);
+  color: #fff;
+  text-align: right;
+  font-family: 'Noto Sans KR';
+  font-size: 1.2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
+import { MarkerInfo } from '../components';
+import { renderToString } from 'react-dom/server';
 
 export const Map = () => {
-  let map = null;
+  const strinnng = renderToString(<MarkerInfo />);
+
+  let map: any = null;
   const initMap = () => {
     map = new naver.maps.Map('map', {
       center: new naver.maps.LatLng(37, 127.039573),
@@ -13,6 +17,7 @@ export const Map = () => {
     });
     // 마커들이 담겨있는 배열
     const markers: naver.maps.Marker[] = [];
+    const infowindows: naver.maps.InfoWindow[] = [];
 
     for (let i = 1; i < 20; i++) {
       const marker = new naver.maps.Marker({
@@ -26,8 +31,59 @@ export const Map = () => {
           // anchor: new naver.maps.Point(25, 26),
         },
       });
+      infowindows.push(
+        new naver.maps.InfoWindow({
+          content: strinnng,
+          backgroundColor: 'transparent',
+          borderColor: 'transparent',
+          disableAnchor: false,
+          anchorColor: 'transparent',
+          position: new naver.maps.Point(0, 0).add(10, 10),
+        }),
+      );
       markers.push(marker);
     }
+    for (let i = 0; i < markers.length; i++) {
+      naver.maps.Event.addListener(markers[i], 'click', function (e) {
+        if (infowindows[i].getMap()) {
+          infowindows[i].close();
+        } else {
+          infowindows[i].open(map, markers[i]);
+        }
+      });
+    }
+    // 마커 표시 함수
+    const showMarker = (map: naver.maps.Map, marker: naver.maps.Marker) => {
+      marker.setMap(map);
+    };
+    // 마커 숨김 함수
+    const hideMarker = (marker: naver.maps.Marker) => {
+      marker.setMap(null);
+    };
+    // 마커 업데이트 유/무 판별 함수
+    const updateMarkers = (map: naver.maps.Map, markers: naver.maps.Marker[]) => {
+      const mapBounds: any = map.getBounds();
+      for (let i = 0; i < markers.length; i++) {
+        const position = markers[i].getPosition();
+        if (mapBounds.hasLatLng(position)) {
+          showMarker(map, markers[i]);
+        } else {
+          hideMarker(markers[i]);
+        }
+      }
+    };
+    // 지도 줌 인/아웃 시 마커 업데이트 이벤트 핸들러
+    naver.maps.Event.addListener(map, 'zoom_changed', () => {
+      if (map !== null) {
+        updateMarkers(map, markers);
+      }
+    });
+    // 지도 드래그 시 마커 업데이트 이벤트 핸들러
+    naver.maps.Event.addListener(map, 'dragend', () => {
+      if (map !== null) {
+        updateMarkers(map, markers);
+      }
+    });
   };
   useEffect(() => {
     initMap();
@@ -37,7 +93,6 @@ export const Map = () => {
     width: '100%',
     height: '90%',
   };
-
   return (
     <>
       <div id="map" style={mapStyle} />
