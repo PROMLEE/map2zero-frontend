193146501215dc08277b56c2d65913cd54acc995
PROMLEE <donghoon099@naver.com>
2024-01-27 16:33:30 +0900

    feat: 다중 마커 생성 Related to: [new feature] 맵 페이지 생성 #38

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,30 +1,41 @@
 import { useEffect } from 'react';
 
 export const Map = () => {
-  useEffect(() => {
-    let map = null;
-    let marker = null;
-    const initMap = () => {
-      map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(37, 127.039573),
-        zoom: 13,
-      });
-      marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(37, 127.039573),
+  let map = null;
+  const initMap = () => {
+    map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37, 127.039573),
+      zoom: 13,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.RIGHT_CENTER,
+      },
+    });
+    // 마커들이 담겨있는 배열
+    const markers: naver.maps.Marker[] = [];
+
+    for (let i = 1; i < 20; i++) {
+      const marker = new naver.maps.Marker({
         map: map,
+        position: new naver.maps.LatLng(37 + i / 100, 127.039573 + i / 100),
         icon: {
-          content: `
-            <img alt="marker" src={vectorIcon} /> //마커에 사용할 이미지
-          `,
+          url: `${process.env.PUBLIC_URL}/assets/Map/marker.svg`,
+          scaledSize: new naver.maps.Size(35, 35),
+          // size: new naver.maps.Size(35, 35),
+          // origin: new naver.maps.Point(0, 0),
+          // anchor: new naver.maps.Point(25, 26),
         },
       });
-    };
+      markers.push(marker);
+    }
+  };
+  useEffect(() => {
     initMap();
   }, []);
 
   const mapStyle = {
     width: '100%',
-    height: '100%',
+    height: '90%',
   };
 
   return (
