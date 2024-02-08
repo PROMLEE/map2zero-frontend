669b0846301f2d9bb5dbf5c7d5a16500d8000093
PROMLEE <donghoon099@naver.com>
2024-02-08 23:42:53 +0900

    fix: mobiletopbar 추가

--- src/components/Mypage/MoreDetails.tsx
+++ src/components/Mypage/MoreDetails.tsx
@@ -2,101 +2,75 @@ import React from 'react';
 import { BookMarkDummy } from './Dummy/BookMarkDummy';
 import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
+import Mobiletop from '../Mobiletop';
 
 type ownerProps = {
   owner?: boolean;
 };
-const MoreDetails = ({owner} : ownerProps) => {
+const MoreDetails = ({ owner }: ownerProps) => {
   const url = owner ? 'ownerUrl' : 'defaultUrl';
   const navigate = useNavigate();
   const onClickBookMark = () => {
     navigate(`/store`);
-  }
+  };
 
   return (
-    <Wrap>
-      <div>
-      <BookMarkTitle> 내가 북마크한 매장</BookMarkTitle>
-      </div>
-      <BookMarks onClick={onClickBookMark}>
-        {BookMarkDummy.map((i) => (
-          <BookMark key={i.storeName}>
-            <StoreImg src={`${process.env.PUBLIC_URL}/assets/MyPage/${i.photo}`} alt={`${i.storeName}의 이미지`} />
-            <BookMarkIcon src={`${process.env.PUBLIC_URL}/assets/MyPage/bookmark.png`} alt="북마크아이콘" />
-            <h3>{i.storeName}</h3>
-            <p>{i.address}</p>
-          </BookMark>
-        ))}
-      </BookMarks>
-    </Wrap>
+    <>
+      <Mobiletop pagename="내가 북마크한 매장" />
+      <Wrap>
+        <BookMarkTitle> 내가 북마크한 매장</BookMarkTitle>
+        <BookMarks onClick={onClickBookMark}>
+          {BookMarkDummy.map((i) => (
+            <BookMark key={i.storeName}>
+              <StoreImg src={`${process.env.PUBLIC_URL}/assets/MyPage/${i.photo}`} alt={`${i.storeName}의 이미지`} />
+              <BookMarkIcon src={`${process.env.PUBLIC_URL}/assets/MyPage/bookmark.png`} alt="북마크아이콘" />
+              <h3>{i.storeName}</h3>
+              <p>{i.address}</p>
+            </BookMark>
+          ))}
+        </BookMarks>
+      </Wrap>
+    </>
   );
 };
 
 const Wrap = styled.div`
-  width: 80%;
-  justify-content: center;
+  width: 92.4rem;
+  margin: 0 auto;
+  padding-bottom: 20rem;
   @media (max-width: 768px) {
-    //width: calc(100vw - 5%);
-     width: 310px;
-     margin: 5%;
-    > div {
-      margin-top: 6rem;
-
-    }
-  }
-
-  > div {
-    margin-top: 40px;
-    //margin-left: 27.5rem;
-    margin-left: auto;
-    display: flex;
-    justify-content: flex-start;
+    width: 85%;
   }
 `;
 
-const BookMarkTitle = styled.h1`
-  margin: 0 0 0 26rem;
-  font-size: 1.4rem;
+const BookMarkTitle = styled.div`
+  margin-top: 40px;
+  font-size: 1.6rem;
   padding-top: 2rem;
-  justify-content: center;
-  align-items: center;
-  
+  font-weight: 600;
   @media (max-width: 768px) {
     display: none;
   }
 `;
 
 const BookMarks = styled.div`
+  margin-top: 40px;
   width: 928px;
-  height: 700px;
-  padding: 0 0 3rem 1rem;
   flex-wrap: wrap;
   align-items: center;
-
-
-
-  /* 인터넷 익스플로러를 위한 스타일 */
-  -ms-overflow-style: none;
-
-  /* 파이어폭스를 위한 스타일 */
-  scrollbar-width: none;
-
-  /* 웹킷(크롬, 사파리, 새로운 엣지) 브라우저를 위한 스타일 */
+  display: flex;
+  justify-content: flex-start;
+  gap: 2rem;
   &::-webkit-scrollbar {
     display: none;
   }
   @media (max-width: 768px) {
-    width: 70rem;
-    height: 100rem;
-    padding: 5rem 0 5rem 2rem;
-    margin: 0;
-   //overflow-x: auto;
+    width: 100%;
+    gap: 2%;
   }
 `;
 
 const BookMark = styled.div`
- 
-  margin: 0rem 2rem 2rem 0;
   width: 16.2rem;
   height: 23rem;
   border: solid;
@@ -120,10 +94,9 @@ const BookMark = styled.div`
     color: rgba(86, 86, 86, 1);
   }
   @media (max-width: 768px) {
-    width: 30rem;
+    width: 49%;
     height: 42rem;
-    margin-right: 4rem;
-
+    margin-bottom: 2rem;
     & > h3 {
       font-size: 3rem;
       margin: 1.5rem;
