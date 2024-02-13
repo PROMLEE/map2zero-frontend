e8e9a73a127fbc02fdab5a19d70de846b8048abc
PROMLEE <donghoon099@naver.com>
2024-02-13 17:15:30 +0900

    feat: 지도 페이지 마커 api 연동

---
+++ src/apis/Map/Markers.ts
@@ -0,0 +1,14 @@
+import { authAPI } from '../customApi';
+
+const Markers = async (myLocation: { latitude: number; longitude: number }) => {
+  try {
+    const response = await authAPI.get(`/stores/map`, { params: { x: myLocation.longitude, y: myLocation.latitude } });
+    console.log(response);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    // alert('연동 에러');
+  }
+};
+
+export default Markers;
