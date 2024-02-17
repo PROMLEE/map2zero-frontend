20307ef1e4a525c538f2a1b03ba6bef850c7533b
PROMLEE <donghoon099@naver.com>
2024-02-17 23:51:00 +0900

    fix: conflit 해결

--- src/recoil/session.ts
+++ src/recoil/session.ts
@@ -1,15 +1,13 @@
-import { selector } from "recoil";
-import sessionApi from "../apis/sessionApi";
+import { selector } from 'recoil';
+import sessionApi from '../apis/SessionApi';
 
-const session= selector< any>({
-  key: "Session",
-  get : async () => {
+const session = selector<any>({
+  key: 'Session',
+  get: async () => {
     const data = await sessionApi();
-    localStorage.setItem('accessToken', data?.headers['authorization'])
+    localStorage.setItem('accessToken', data?.headers['authorization']);
     const accessToken = data?.headers['authorization'];
-  }
-
+  },
 });
 
-
-export { session};
+export { session };
